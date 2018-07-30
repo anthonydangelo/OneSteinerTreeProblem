a7960f20aa1ad88614a9a8719f3e2a13ec27f76e
Anthony <anthony.dangelo@carleton.ca>
2018-07-30 16:32:33 -0400

    set up with vs code

--- src/1ST-Constants-Utilities.h
+++ src/1ST-Constants-Utilities.h
@@ -1,5 +1,5 @@
 #ifndef ONE_ST_CONSTANTS_UTILITIES
-#define 	ONE_ST_CONSTANTS_UTILITIES
+#define ONE_ST_CONSTANTS_UTILITIES
 
 //magic numbers
 #define MIN_NUM_INPUT_POINTS (3)
@@ -7,6 +7,8 @@
 
 #define RAND_SEED			 (10)
 #define TRUE_RANDOM_DEFAULT  (false)
+
+#define ONLY_POINTS_DEFAULT  (false)
 
 
 #endif
--- src/OneSteinerTreeProblem.cpp
+++ src/OneSteinerTreeProblem.cpp
@@ -6,56 +6,98 @@
 // Description : Implementation of Georgakopolous and Papadimitriou's O(n^2) 1 Steiner tree algorithm
 //============================================================================
 
-#include <boost/program_options.hpp>
-#include <boost/program_options/options_description.hpp>
-#include <boost/optional/optional.hpp>
+#include <getopt.h>
 #include <iostream>
 #include <sstream> // for ostringstream
 #include <string>
 
+#include <iostream>
+#include <unistd.h>
+#include <cstdlib>
+
 #include "1ST-Constants-Utilities.h"
 
 using namespace std;
-namespace po = boost::program_options;
+
+void PrintHelp()
+{
+	ostringstream msg;
+	msg <<
+			"--onlyPoints:        Only create new random points and return\n"
+            "--numPoints <int>:   Number of random points to generate (between "
+					<< MIN_NUM_INPUT_POINTS << " and " << MAX_NUM_INPUT_POINTS << ", default is "
+					<< MIN_NUM_INPUT_POINTS << ")\n"
+            "--trueRandom:        Seed the rand. num. gen. with the current time rather than a fixed seed (default is "
+					<< TRUE_RANDOM_DEFAULT << ")\n"
+            "--randSeed <int>:    Use the following fixed seed for the rand. num. gen. (default is " << RAND_SEED << ")\n"
+            "--help:              Show help\n";
+
+	cout << msg.str();
+    exit(1);
+}
+
+int numPoints = MIN_NUM_INPUT_POINTS;
+int randSeed = RAND_SEED;
+bool trueRandom = TRUE_RANDOM_DEFAULT;
+bool onlyPoints = ONLY_POINTS_DEFAULT;
+
+//https://codeyarns.com/2015/01/30/how-to-parse-program-options-in-c-using-getopt_long/
+void ProcessArgs(int argc, char** argv)
+{
+    const char* const short_opts = "pn:rs:h";
+    const option long_opts[] = {
+            {"onlyPoints", no_argument, nullptr, 'p'},
+            {"numPoints", required_argument, nullptr, 'n'},
+            {"trueRandom", no_argument, nullptr, 'r'},
+            {"randSeed", required_argument, nullptr, 's'},
+            {"help", no_argument, nullptr, 'h'},
+            {nullptr, no_argument, nullptr, 0}
+    };
+
+    //note: string(optarg) is to take in and make a string
+    while (true)
+    {
+        const auto opt = getopt_long(argc, argv, short_opts, long_opts, nullptr);
+
+        if (-1 == opt)
+            break;
+
+        switch (opt)
+        {
+        case 'p':
+        	onlyPoints = true;
+        	break;
+
+        case 'n':
+        	if(optarg){
+        		numPoints = stoi(optarg);
+        	}
+            break;
+
+        case 'r':
+        	trueRandom = true;
+            break;
+
+        case 's':
+        	if(optarg){
+        		//note: also stof exists
+        		randSeed = stoi(optarg);
+        	}
+            break;
+
+        case 'h': // -h or --help
+        case '?': // Unrecognized option
+        default:
+            PrintHelp();
+            break;
+        }
+    }
+    return;
+}
 
 int main(int argc, char** argv) {
 
-	int numPoints = MIN_NUM_INPUT_POINTS;
-	int randSeed = RAND_SEED;
-	bool trueRandom = TRUE_RANDOM_DEFAULT;
-	boost::optional<int> nPts;
-	boost::optional<int> rSeed;
-	boost::optional<bool> trueRndm;
-
-	ostringstream numPointsMsg, randSeedMsg, trueRandomMsg;
-	numPointsMsg << "number of random points to use (from " << MIN_NUM_INPUT_POINTS << " to " << MAX_NUM_INPUT_POINTS << ")";
-	randSeedMsg << "random num. gen. seed to use (default is " << RAND_SEED << ")";
-	trueRandomMsg << "false if we should use a fixed seed for the random num. gen. (default is " << TRUE_RANDOM_DEFAULT << ")";
-
-	po::options_description desc("Options");
-
-	desc.add_options()
-				("help, h", "produce help message")
-				("trueRandom, r", po::value<bool>(&trueRndm)->default_value(TRUE_RANDOM_DEFAULT), trueRandomMsg.str())
-	            ("numPoints, n", po::value<int>(&nPts)->default_value(MIN_NUM_INPUT_POINTS), numPointsMsg.str())
-	            ("randSeed, s", po::value<int>(&rSeed)->default_value(MIN_NUM_INPUT_POINTS), randSeedMsg.str())
-	            ("filename", po::value<std::string>()->default_value(""));
-
-
-	if(nPts){
-		numPoints = *nPts;
-	}
-	if(rSeed){
-		randSeed = *rSeed;
-	}
-	if(trueRndm){
-		trueRandom = *trueRndm;
-	}
-
-
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc,argv,desc),vm);
-    po::notify(vm);
+	 ProcessArgs(argc, argv);
 
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
